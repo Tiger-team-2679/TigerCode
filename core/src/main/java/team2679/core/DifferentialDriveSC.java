242fab64c9717be1cffaf5f9de1e51be3cb726c1
Yoav <51864749+yo314159at@users.noreply.github.com>
2020-01-20 17:43:57 +0200

    Add documantation for DifferentialDriveSC and MotionProfileGenerator

--- core/src/main/java/team2679/core/DifferentialDriveSC.java
+++ core/src/main/java/team2679/core/DifferentialDriveSC.java
@@ -3,18 +3,30 @@ package team2679.core;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Creates a graph of the average speed between the wheels for the robot against distance considering the robot's max speed.
+ */
 public class DifferentialDriveSC {
 
     double width;
     double maxVelocity;
     int dataPoints;
 
+    /**
+     * @param width The space between the wheels of the robot.
+     * @param maxVelocity The fastest the robot can travel.
+     * @param dataPoints The amount of samples the graph will have on the x-axis.
+     */
     public DifferentialDriveSC(double width, double maxVelocity, int dataPoints) {
         this.width = width;
         this.maxVelocity = maxVelocity;
         this.dataPoints = dataPoints;
     }
 
+    /**
+     * @param spline A spline of the robot's path.
+     * @return A graph of the average speed between the wheels for the robot against distance considering the robot's max speed.
+     */
     public Graph getSpeedCap(ExtendedSpline spline) {
         Map points = new HashMap();
         double length = 0;
--- core/src/main/java/team2679/core/MotionProfileGenerator.java
+++ core/src/main/java/team2679/core/MotionProfileGenerator.java
@@ -3,20 +3,32 @@ package team2679.core;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Takes a graph that was speed capped, and limits it based on max acceleration and deceleration.
+ */
 public class MotionProfileGenerator {
 
     private double acceleration;
     private double step;
-    private double lastVelocity;
 
+    /**
+     * @param acceleration The fastest the robot can accelerate and decelerate.
+     * @param step The amount of space traveled between samples to make the graph. (The lower the number, the more accurate it will be)
+     */
     public MotionProfileGenerator(double acceleration, double step) {
         this.acceleration = acceleration;
         this.step = step;
     }
 
+    /**
+     * @param speedCap A graph of the average speed of the robot against distance limited to the robot's max speed.
+     * @param startVelocity The speed of the robot at the start of the path.
+     * @param endVelocity The speed of the robot at the end of the path.
+     * @return A graph that was changed based on the original to have realistic acceleration and deceleration.
+     */
     public Graph generate(Graph speedCap, double startVelocity, double endVelocity) {
         Map points = new HashMap();
-        lastVelocity = startVelocity;
+        double lastVelocity = startVelocity;
         for (double distance = 0; distance < speedCap.getRange().getMaximum(); distance += step) {
             double currentVelocity = Math.sqrt(lastVelocity * lastVelocity + 2 * acceleration * distance);
             currentVelocity = Math.min(currentVelocity, speedCap.value(distance));
@@ -25,6 +37,7 @@ public class MotionProfileGenerator {
         }
         Graph accelerate = new Graph(points);
         points = new HashMap();
+        lastVelocity = endVelocity;
         for (double distance = accelerate.getRange().getMaximum(); distance > 0; distance -= step) {
             double currentVelocity = Math.sqrt(lastVelocity * lastVelocity + 2 * acceleration * distance);
             currentVelocity = Math.min(currentVelocity, accelerate.value(distance));
